ab2ae42efe7f292e86c3dc18bdc3c8661651293d
Rouguiya <rouguiyatout@gmail.com>
2026-03-01 15:38:51 +0000

    Ajout cle SSH EC2

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,15 @@
 # terraform/main.tf
 
+resource "aws_key_pair" "devops_key" {
+  key_name   = "devops-key"
+  public_key = file("${path.module}/../keySSH_ProjetFilRouge.pub")
+}
+
 resource "aws_instance" "devops_ec2" {
-  ami                    = "ami-09d0c9a85bf1b9ea7"  
-  instance_type          = "t3.micro"               
+  ami                    = "ami-09d0c9a85bf1b9ea7"
+  instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.devops_sg.id]
+  key_name               = aws_key_pair.devops_key.key_name
 
   user_data = file("user_data.sh")
 
